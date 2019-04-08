f4563a1a7b62ec618f5bc35888eeb796539bc6da
sungwon.cha <mygenie3@naver.com>
2019-04-08 23:31:13 +0900

    fix

---
+++ documentation/resources/data/fake-users.js
@@ -0,0 +1,49 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import faker from 'faker';
+
+const generate = index => ({
+  // id: index,
+  avatar: faker.image.avatar(),
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  city: faker.address.city(),
+  email: faker.internet.email(),
+  street: faker.address.streetName(),
+  zipCode: faker.address.zipCode(),
+  date: faker.date.past(),
+  bs: faker.company.bs(),
+  catchPhrase: faker.company.catchPhrase(),
+  companyName: faker.company.companyName(),
+  words: faker.lorem.words(),
+  sentence: faker.lorem.sentence(),
+});
+
+const columnDef = {
+  id: {
+    width: 50,
+  },
+  avatar: {
+    width: 50,
+    render: src => <img src={src} height={50} alt="avatar" />,
+  },
+};
+
+const getFakeData = (size = 1000) => {
+  const rows = new Array(size).fill(true).map((e, i) => generate(i));
+  const columns = Object.keys(rows[0]).map((name) => {
+    if (columnDef[name]) {
+      return { name, ...columnDef[name] };
+    }
+    return name;
+  });
+  return { columns, rows };
+};
+
+const fakeUsers = getFakeData();
+
+const { columns, rows } = fakeUsers;
+
+export default fakeUsers;
+
+export { columns, rows, getFakeData };
---
+++ documentation/resources/data/pokedex.js
@@ -0,0 +1,9 @@
+const { pokemons } = require('./pokedex.json');
+
+const columns = pokemons
+  .reduce((prev, e) => [...prev, ...Object.keys(e)], [])
+  .filter((v, i, a) => a.indexOf(v) === i);
+
+const rows = pokemons;
+
+export { columns, rows };

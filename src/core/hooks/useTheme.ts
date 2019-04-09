d0e79ddea0ec39d7f312d17feccad0553694245c
sungwon.cha <mygenie3@naver.com>
2019-04-09 21:54:44 +0900

    fix

--- src/core/hooks/useTheme.ts
+++ src/core/hooks/useTheme.ts
@@ -4,41 +4,42 @@ import { ClassNames } from './useClassNames';
 
 export type ThemeFunction = (classNames: ClassNames) => StringAnyMap;
 
-function defaultTheme(classNames: ClassNames) {
+function defaultTheme(_: ClassNames) {
   return css({
     position: 'relative',
     boxSizing: 'border-box',
-    [classNames.CELL]: {
+    [_.CELL]: {
       boxSizing: 'border-box',
       overflow: 'hidden',
+      background: '#fff',
     },
-    [classNames.GUIDELINE_TOP]: {
+    [_.GUIDELINE_TOP]: {
       height: 5,
       background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0) 100%)',
     },
-    [classNames.GUIDELINE_BOTTOM]: {
+    [_.GUIDELINE_BOTTOM]: {
       height: 5,
       background: 'linear-gradient(to top, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0) 100%)',
     },
-    [classNames.GUIDELINE_LEFT]: {
+    [_.GUIDELINE_LEFT]: {
       width: 5,
       background: 'linear-gradient(to right, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0) 100%)',
     },
-    [classNames.GUIDELINE_RIGHT]: {
+    [_.GUIDELINE_RIGHT]: {
       width: 5,
       background: 'linear-gradient(to left, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0) 100%)',
     },
-    [classNames.SCROLL_TOP]: {
-      [classNames.GUIDELINE_TOP]: { opacity: 0 },
+    [_.SCROLL_TOP]: {
+      [_.GUIDELINE_TOP]: { opacity: 0 },
     },
-    [classNames.SCROLL_BOTTOM]: {
-      [classNames.GUIDELINE_BOTTOM]: { opacity: 0 },
+    [_.SCROLL_BOTTOM]: {
+      [_.GUIDELINE_BOTTOM]: { opacity: 0 },
     },
-    [classNames.SCROLL_LEFT]: {
-      [classNames.GUIDELINE_LEFT]: { opacity: 0 },
+    [_.SCROLL_LEFT]: {
+      [_.GUIDELINE_LEFT]: { opacity: 0 },
     },
-    [classNames.SCROLL_RIGHT]: {
-      [classNames.GUIDELINE_RIGHT]: { opacity: 0 },
+    [_.SCROLL_RIGHT]: {
+      [_.GUIDELINE_RIGHT]: { opacity: 0 },
     },
   });
 }
